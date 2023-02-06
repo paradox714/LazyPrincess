976d382b9ae2de90805bedefab08361d7f90a7b9
paradox714 <79100103+paradox714@users.noreply.github.com>
2023-02-06 20:57:26 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -711,25 +711,37 @@ async def auto_filter(client, msg, spoll=False):
         cap = f"Darling, Here is what i found for your query {search}"
     if imdb and imdb.get('poster'):
         try:          
-            l = await message.reply_photo(photo=imdb.get('poster'), caption=cap[:1024],await asyncio.sleep(300),await l.delete()
-
-           
-
-           
-
-               reply_markup=InlineKeyboardMarkup(btn))
-
+            l = await message.reply_photo(photo=imdb.get('poster'), caption=cap[:1024], 
+                              reply_markup=InlineKeyboardMarkup(btn))
         except (MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty):
+
             pic = imdb.get('poster')
+
             poster = pic.replace('.jpg', "._V1_UX360.jpg")
+
             await message.reply_photo(photo=poster, caption=cap[:1024], reply_markup=InlineKeyboardMarkup(btn))
+
         except Exception as e:
+
             logger.exception(e)
+
             await message.reply_text(cap, reply_markup=InlineKeyboardMarkup(btn))
+
     else:
+
         await message.reply_text(cap, reply_markup=InlineKeyboardMarkup(btn))
+
     if spoll:
+
         await msg.message.delete()
+        
+           
+
+               
+                                        
+                                          
+
+        
 
 
 async def advantage_spell_chok(msg):
